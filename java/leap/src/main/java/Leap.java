dec60f9eb949dc12f3a50640fc2126930bc1c4aa
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2019-12-06 00:12:41 +0000

    Completed Leap year challenge

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,11 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 ==0)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
