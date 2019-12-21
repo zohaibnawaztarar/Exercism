804e1fe16a3710bf1f609d472217d7a22ce1c5ec
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2019-12-21 12:32:13 +0000

    Revert "Completed Resistor Color challenge"
    This reverts commit 931785ee00861c8260586674d43cace32d33784e.

--- csharp/gigasecond/Gigasecond.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class Gigasecond
-{
-    public static DateTime Add(DateTime moment)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-}
